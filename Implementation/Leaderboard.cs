8d06c3b874737fa2d478c763ff704667f9d6b76a
Johannes Ebner <Structed@users.noreply.github.com>
2022-11-16 17:06:46 +0100

    Add first implementation for Leaderboard service/Repo

---
+++ Implementation/Leaderboard.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection.PortableExecutable;
+using StackExchange.Redis;
+
+namespace LevelLeaderboards.Implementation;
+
+public class Leaderboard
+{
+    private readonly IConnectionMultiplexer connectionMultiplexer;
+
+    public Leaderboard(IConnectionMultiplexer connectionMultiplexer)
+    {
+        this.connectionMultiplexer = connectionMultiplexer;
+    }
+    public void AddEntry(string levelId, string titlePlayerId, int score)
+    {
+        // Fire and forget
+        _ = connectionMultiplexer.GetDatabase().SortedSetAddAsync(this.GetKey(levelId), titlePlayerId, score);
+    }
+
+    public long GetRank(string levelId, string titlePlayerId)
+    {
+        var result = connectionMultiplexer.GetDatabase().SortedSetRank(GetKey(levelId), titlePlayerId, Order.Descending);
+        var rank = result.GetValueOrDefault();
+
+        return rank;
+    }
+
+    private RedisKey GetKey(string levelId)
+    {
+        return $"leaderboard:{levelId}";
+    }
+}
