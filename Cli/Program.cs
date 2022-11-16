a7966604edbcdba6b19a373ffc117f755abf7931
Johannes Ebner <Structed@users.noreply.github.com>
2022-11-16 17:06:22 +0100

    Add new projects

---
+++ Cli/Program.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+
+namespace LevelLeaderboards.Cli
+{
+    class Program
+    {
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("level-leaderboards.redis.cache.windows.net:6380,password=Ix5JlphGLPRq77qbSyNNu2bG7xQWLKNeaAzCaNT1guk=,ssl=True,abortConnect=False");
+        static async Task Main(string[] args)
+        {
+            var db = redis.GetDatabase();
+            var pong = await db.PingAsync();
+            Console.WriteLine(pong);
+        }
+    }
+}
+
