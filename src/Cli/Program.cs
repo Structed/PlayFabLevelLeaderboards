e77a139ad2060997849837417a1d045b118cc9e4
Johannes Ebner <Structed@users.noreply.github.com>
2022-11-18 13:56:03 +0100

    Removed connection string

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -4,7 +4,7 @@ namespace LevelLeaderboards.Cli
 {
     class Program
     {
-        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("level-leaderboards.redis.cache.windows.net:6380,password=Ix5JlphGLPRq77qbSyNNu2bG7xQWLKNeaAzCaNT1guk=,ssl=True,abortConnect=False");
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING"));
         static async Task Main(string[] args)
         {
             var db = redis.GetDatabase();
