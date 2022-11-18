0af96b821d6d1deb8166897beafe487c847c2f60
Johannes Ebner <Structed@users.noreply.github.com>
2022-11-18 17:15:50 +0100

    Implemented Azure Function to store an entry

---
+++ src/LevelLeaderboards.Web/PostScore.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using LevelLeaderboards.Implementation;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace LevelLeaderboards.Web;
+
+public class PostScore
+{
+    private readonly ILeaderboardRepository leaderboardRepository;
+
+    public PostScore(ILeaderboardRepository leaderboardRepository)
+    {
+        this.leaderboardRepository = leaderboardRepository;
+    }
+
+    [Function("PostScore")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "add/levelId/{levelId}/titlePlayerId/{titlePlayerId}/score/{score:int}")] HttpRequestData req, string levelId, string titlePlayerId, int score,
+        FunctionContext executionContext)
+    {
+         this.leaderboardRepository.AddEntry(levelId, titlePlayerId, score);
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+
+        return response;
+
+    }
+}
--- src/LevelLeaderboards.Web/Program.cs
+++ src/LevelLeaderboards.Web/Program.cs
@@ -1,7 +1,22 @@
+using LevelLeaderboards.Implementation;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
+
+var redis = ConnectionMultiplexer.Connect(
+    Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING") ?? throw new ArgumentNullException(
+        "REDIS_CONNECTION_STRING",
+        "Environment Variable not set"
+    ));
+var db = redis.GetDatabase();
+var leaderboardRepository = new LeaderboardRepository(redis);
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices( services =>
+    {
+        services.AddSingleton<ILeaderboardRepository>(leaderboardRepository);
+    })
     .Build();
 
 host.Run();
